c97236fcc34eeff7c3bd59f2812979bb2e4e52e9
AlessandroLuisglitch <francapesquisas@gmail.com>
2026-01-29 17:09:44 +0000

    arrumando bug da foto de perfil

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  // substituir a imagem
   if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/fotoperfil.jpeg")
   } else {
-    // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./fotoperfil.jpeg")
+    // Agora o caminho aponta corretamente para a pasta assets
+    img.setAttribute("src", "./assets/fotoperfil.jpeg")
   }
 }
